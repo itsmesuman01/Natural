b7033ccf20071345b810a338f63bb825d1591d09
fyp-preparation <sumanpandey977nep@gmail.com>
2024-09-02 09:41:36 +0545

    Pages & Components Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+import Topbar from './components/Topbar.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Topbar />
     <App />
+    <Footer />
   </StrictMode>,
 )
