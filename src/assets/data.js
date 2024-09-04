749c10c21a6f2415fa038a4d4b50a801bfe0e951
fyp-preparation <sumanpandey977nep@gmail.com>
2024-09-04 16:02:55 +0545

    Data.js File Created

---
+++ src/assets/data.js
@@ -0,0 +1,18 @@
+import { carasoul } from './assets'
+export const about = [
+  {
+    image: carasoul.wallpaper_one,
+    header: 'Beautiful Himalaya',
+    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasisaepe exercitationem consequatur eum debitis minima. Non quos hicunde aliquam quia perspiciatis ipsa ex ad repudiandae? Impeditquidem eligendi voluptatibus eaque, culpa corporis consequaturducimus, debitis quos sequi reiciendis iste magnam distinctio undeaut, quo sit in itaque quasi fugit nam quam nobis voluptatemprovident? Cupiditate assumenda, exercitationem repudiandae nostrumrepellat omnis praesentium delectus atque eaque sunt cum,recusandae, quam a excepturi aut numquam itaque laudantium ad nihilexpedita alias ratione earum? Nostrum accusantium sequi culpa isteobcaecati unde illo amet vero minima cumque, delectus aliquid, aexercitationem in eligendi.',
+  },
+  {
+    image: carasoul.wallpaper_two,
+    header: 'Beautiful Hill',
+    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasisaepe exercitationem consequatur eum debitis minima. Non quos hicunde aliquam quia perspiciatis ipsa ex ad repudiandae? Impeditquidem eligendi voluptatibus eaque, culpa corporis consequaturducimus, debitis quos sequi reiciendis iste magnam distinctio undeaut, quo sit in itaque quasi fugit nam quam nobis voluptatemprovident? Cupiditate assumenda, exercitationem repudiandae nostrumrepellat omnis praesentium delectus atque eaque sunt cum,recusandae, quam a excepturi aut numquam itaque laudantium ad nihilexpedita alias ratione earum? Nostrum accusantium sequi culpa isteobcaecati unde illo amet vero minima cumque, delectus aliquid, aexercitationem in eligendi.',
+  },
+  {
+    image: carasoul.wallpaper_three,
+    header: 'Beautiful River',
+    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasisaepe exercitationem consequatur eum debitis minima. Non quos hicunde aliquam quia perspiciatis ipsa ex ad repudiandae? Impeditquidem eligendi voluptatibus eaque, culpa corporis consequaturducimus, debitis quos sequi reiciendis iste magnam distinctio undeaut, quo sit in itaque quasi fugit nam quam nobis voluptatemprovident? Cupiditate assumenda, exercitationem repudiandae nostrumrepellat omnis praesentium delectus atque eaque sunt cum,recusandae, quam a excepturi aut numquam itaque laudantium ad nihilexpedita alias ratione earum? Nostrum accusantium sequi culpa isteobcaecati unde illo amet vero minima cumque, delectus aliquid, aexercitationem in eligendi.',
+  }
+];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,70 +1,30 @@
 import React from "react";
-import { carasoul } from "../assets/assets";
+import { about } from "../assets/data";
 
 const About = () => {
   return (
     <div>
-      <div className="flex text-black mt-1 mb-1">
-        <div className="left flex-1 animate-fadeIn">
-          <img
-            className="w-full h-full object-cover"
-            src={carasoul.wallpaper_one}
-            alt="image"
-          />
-        </div>
-        <div className="right flex-1 animate-fadeIn p-10">
-          <div className="text-3xl md:text-5xl mt-5 mb-5 flex justify-center">
-            Beautiful Himalaya
+      <div className="flex flex-col text-black mt-1 mb-1">
+        {about.map((item, index) => (
+          <div key={index} className="flex flex-col sm:flex-row mb-4">
+            <div className="flex-1 animate-fadeIn">
+              <img
+                className="w-full h-auto object-cover"
+                src={item.image}
+                alt="About image"
+              />
+            </div>
+            <div className="flex-1 animate-fadeIn p-10">
+              <div className="text-3xl md:text-5xl mt-5 mb-5 flex justify-center">
+                {item.header}
+              </div>
+              <div className="flex justify-center my-10">
+                <hr className="w-60 border-t-2 border-gray-100" />
+              </div>
+              <p className="text-center">{item.content}</p>
+            </div>
           </div>
-          <div className="flex justify-center my-10">
-            <hr className="w-60 border-t-2 border-gray-100" />
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
-            saepe exercitationem consequatur eum debitis minima. Non quos hic
-            unde aliquam quia perspiciatis ipsa ex ad repudiandae? Impedit
-            quidem eligendi voluptatibus eaque, culpa corporis consequatur
-            ducimus, debitis quos sequi reiciendis iste magnam distinctio unde
-            aut, quo sit in itaque quasi fugit nam quam nobis voluptatem
-            provident? Cupiditate assumenda, exercitationem repudiandae nostrum
-            repellat omnis praesentium delectus atque eaque sunt cum,
-            recusandae, quam a excepturi aut numquam itaque laudantium ad nihil
-            expedita alias ratione earum? Nostrum accusantium sequi culpa iste
-            obcaecati unde illo amet vero minima cumque, delectus aliquid, a
-            exercitationem in eligendi.
-          </p>
-        </div>
-      </div>
-      <div className="flex text-black mt-1 mb-1">
-        <div className="left flex-1 animate-fadeIn">
-          <img
-            className="w-full h-full object-cover"
-            src={carasoul.wallpaper_two}
-            alt="image"
-          />
-        </div>
-        <div className="right flex-1 animate-fadeIn p-10">
-          <div className="text-3xl md:text-5xl mt-5 mb-5 flex justify-center">
-            Beautiful Hill
-          </div>
-          <div className="flex justify-center my-10">
-            <hr className="w-60 border-t-2 border-gray-100" />
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
-            saepe exercitationem consequatur eum debitis minima. Non quos hic
-            unde aliquam quia perspiciatis ipsa ex ad repudiandae? Impedit
-            quidem eligendi voluptatibus eaque, culpa corporis consequatur
-            ducimus, debitis quos sequi reiciendis iste magnam distinctio unde
-            aut, quo sit in itaque quasi fugit nam quam nobis voluptatem
-            provident? Cupiditate assumenda, exercitationem repudiandae nostrum
-            repellat omnis praesentium delectus atque eaque sunt cum,
-            recusandae, quam a excepturi aut numquam itaque laudantium ad nihil
-            expedita alias ratione earum? Nostrum accusantium sequi culpa iste
-            obcaecati unde illo amet vero minima cumque, delectus aliquid, a
-            exercitationem in eligendi.
-          </p>
-        </div>
+        ))}
       </div>
     </div>
   );
